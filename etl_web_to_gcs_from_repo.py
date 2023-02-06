9e7c50d93ad3a2e34d44bb40dc35e6d829c3e6fd
Denis Bosiak <b.denis92@gmail.com>
2023-02-06 16:26:46 +0200

    Add etl_web_to_gcs_from_repo

--- etl_web_to_gcs_from_repo.py
+++ etl_web_to_gcs_from_repo.py
@@ -29,7 +29,7 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
-    path = Path(f"flows/data/{color}/{dataset_file}.parquet")
+    path = Path(f"data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
 
