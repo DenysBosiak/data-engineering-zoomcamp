9c2f64340acad2edec9b8af422113a8d676a5195
Denis Bosiak <b.denis92@gmail.com>
2023-02-06 16:22:27 +0200

    Add etl_web_to_gcs_from_repo

--- etl_web_to_gcs_from_repo.py
+++ etl_web_to_gcs_from_repo.py
@@ -18,8 +18,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
