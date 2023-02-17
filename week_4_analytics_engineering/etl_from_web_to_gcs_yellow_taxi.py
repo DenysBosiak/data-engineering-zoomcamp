3f6738ae18061cc62f963264b22e0730df4e8b1b
Denis Bosiak <b.denis92@gmail.com>
2023-02-17 11:50:01 +0200

    Add week_4_analytic_engineering

---
+++ week_4_analytics_engineering/etl_from_web_to_gcs_yellow_taxi.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+    df = pd.read_csv(dataset_url, low_memory=False, encoding='latin1')
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.csv")
+    df.to_csv(path, compression="gzip", encoding='utf-8')
+    return path
+
+
+@task()
+def write_gcs(path: Path, color: str, dataset_file: str) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("prefect-gsp")
+    bucket_path = f"data/{color}/{dataset_file}.csv"
+    gcs_block.upload_from_path(from_path=path, to_path=bucket_path)
+    return
+
+
+@flow()
+def etl_web_to_gcs(year: int, month: int, color: str) -> None:
+    """The main ETL function"""
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path, color, dataset_file)
+
+
+@flow
+def etl_parent(color: str, months: list[int], years: list[int]):
+    """The parent ETL function"""
+    for year in years:
+        for month in months:
+            etl_web_to_gcs(year, month, color)
+
+
+if __name__ == "__main__":
+    years = [2019,2020]
+    months = list(range(2,12+1))
+    color = 'yellow'
+    etl_parent(color, months, years)
